b733fc84e7d3cadaa22ade295f62cc3240ec4dbd
Dev <devawal5@gmail.com>
2020-10-22 14:18:17 +0100

    Fix issue with modal by replacing with alert

--- react-app/src/LoginPage.jsx
+++ react-app/src/LoginPage.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
-import React, { Component, useState } from "react";
-import { Button, FormGroup, FormControl, ControlLabel, Modal } from "react-bootstrap";
-import { render } from "react-dom";
+import React, {useState } from "react";
+import { Button, FormGroup, FormControl, ControlLabel} from "react-bootstrap";
 import "./Login.css";
 
 export default function Login() {
@@ -9,49 +8,29 @@ export default function Login() {
   let [user, setUser] = useState("");
   const [password, setPassword] = useState("");
 
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
   const [isAuth, setAuth] = useState(false);
 
   function validateForm() {
     return user.length > 0 && password.length > 0;
   }
 
-    function shouldComponentUpdate(nextProps, nextState) {
-    if (this.props.isAuth === nextProps.isAuth) {
-        return false;
-      } else {
-        return true;
-      }
-    }
 
-  function handleSubmit(event) {
-    // axios.post("http://127.0.0.1/login", {
-    //     user,
-    //     password
-    // }).then(result => {
-    //     if (result.status === 200) {
-    //         setAuth(true);
-    //         console.log('200')
-    //     }
-    // }).catch(e => {
-    //     console.log('Got error')
-    //     setShow(true);
-    // })
-    setShow(true);
-    event.preventDetfault();
+  function handleSubmit(event) { 
+    axios.post("http://127.0.0.1/login", {
+        user,
+        password
+    }).then(result => {
+        if (result.status === 200) {
+            setAuth(true);
+            console.log('200')
+        }
+    }).catch(e => {
+        alert('Incorrect Username or Password, please try again')
+    });
+    
   }
   return (
     <div className="Login">
-        <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        </Modal>
       <form onSubmit={handleSubmit}>
         <FormGroup controlId="username" bsSize="large">
           <ControlLabel>Username</ControlLabel>
