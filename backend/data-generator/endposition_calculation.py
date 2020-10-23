a4b925f575dc7189d401e1f60917d934592b34e5
Maxim Mansurow <m.mansurow@hotmail.de>
2020-10-23 08:59:06 +0100

    creating profit_loss_calculation python script. Requirement No. 8

--- backend/data-generator/endposition_calculation.py
+++ backend/data-generator/endposition_calculation.py
@@ -12,8 +12,6 @@ class endposition:
 
         my_cursor = my_db.cursor()
 
-        #my_cursor.execute("SELECT buys.deal_counterparty_id, total_sells - total_buys as win_or_loss from (SELECT deal_counterparty_id,sum(deal_amount) as total_buys FROM db_grad_cs_1917.deal WHERE deal_type='B' group by deal_counterparty_id) as buys INNER JOIN (SELECT deal_counterparty_id,sum(deal_amount) as total_sells FROM db_grad_cs_1917.deal WHERE deal_type='S' group by deal_counterparty_id) as sells ON buys.deal_counterparty_id=sells.deal_counterparty_id ;")
-
         sql_query = """ 
         SELECT buys.deal_counterparty_id, buys.deal_instrument_id, instrument_name ,
         sum(total_sells) - sum(total_buys) as endposition, sum(sells.total_sell_quantity) - sum(buys.total_buy_quantity) as total_quantity
@@ -30,14 +28,12 @@ class endposition:
         group by buys.deal_counterparty_id, buys.deal_instrument_id ;
         """
 
-
         my_cursor.execute(sql_query)
 
         r = [dict((my_cursor.description[i][0], str(value)) for i, value in enumerate(row)) for row in my_cursor.fetchall()]
 
         return r
 
-        #myresult = my_cursor.fetchall()
 
 
 if __name__ == "__main__":
