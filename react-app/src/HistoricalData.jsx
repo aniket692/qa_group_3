4b63b5043bac2b791df6bf36de86a91ed7277e90
Dev <devawal5@gmail.com>
2020-10-23 09:51:49 +0100

    Replace static html with dynamic variables, need to connect to webtier

--- react-app/src/HistoricalData.jsx
+++ react-app/src/HistoricalData.jsx
@@ -1,37 +1,77 @@
 import axios from "axios";
-import React from "react";
+import React, {useState} from "react";
+import { Button} from "react-bootstrap";
 
 export default function HistoricalData(props){
 
-  let user = props.user
+  const user = props.user
+  let [instrument, setInstrument] = useState("");
+  let [startPeriod, setStartPeriod] = useState("1997-11-14T08:30");
+  let [endPeriod, setEndPeriod] = useState("2060-06-01T08:30"); 
+
+  let [averageBuy, setAverageBuy] = useState(""); 
+  let [averageSell, setAverageSell] = useState(""); 
+
+  const [endPosition, setEndPosition] = useState('Checking...')
+  const [realised, setRealised] = useState('Checking...')
+  const [effective, setEffective] = useState('Checking...')
+
+  axios.get("http://127.0.0.1:8090/dealerdata")
+                          .then((response) => {
+                            console.log(response)
+                          })
+
+
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    axios.post("http://127.0.0.1:8090/instrument", {
+      'instrument': instrument,
+      'startPeriod': startPeriod,
+      'endPeriod': endPeriod
+  }).then(result => {
+      console.log(result)
+  })
+  }
 
   return (
     <div>
-<h2> Welcome {props.user}! </h2>
+<h2> Welcome {user}! </h2>
+
+
+<h2> Dealer Information </h2>
+  <p> End position of dealer: {endPosition} </p>
+  <p> Realised profit or loss of dealer: {realised} </p>
+  <p> Effective profit or loss of dealer: {effective} </p>
+  
+
+
 <h3>Please enter the instrument name and the period you want to search for historic data</h3>
 
 
 <form>
   <label for="instrumentName">Instrument Name:</label>
-  <input type="text" id="instrumentName" name="instrumentName"/><br></br>
+  <input type="text" id="instrumentName" name="instrumentName" value={instrument} onChange={e => setInstrument(e.target.value)}/><br></br>
   <label for="startPeriod">Start Period:</label>
-  <input type="datetime-local" id="startPeriod" name="startPeriod"/><br></br>
+  <input type="datetime-local" id="startPeriod" name="startPeriod" value={startPeriod} onChange={e => setStartPeriod(e.target.value)}/><br></br>
   <label for="endPeriod">End Period:</label>
-  <input type="datetime-local" id="endPeriod" name="endPeriod"/>
+  <input type="datetime-local" id="endPeriod" name="endPeriod" value={endPeriod} onChange={e => setEndPeriod(e.target.value)}/>
 </form>
 
 <p><strong>Note:</strong> This tool is not supported in Firefox, Safari or Internet Explorer 12 (or earlier).</p>
 
-  <input type="submit"/>
+      <Button block bsSize="large" onClick={handleSubmit} type="submit">
+          Submit
+      </Button>
   
-  <h2>Historic data report</h2>
+  {!(instrument === "") && 
+  <div>
+  <h2>Averages for the given instrument {instrument} </h2>
   
-  <p> Average buy price in the period: $averagebuyprice$ </p>
-  <p> Average end price in the period: $averageendprice$ </p>
-  <p> End position of dealer: $endposition$ </p>
-  <p> Realised profit or loss of dealer: $realisedprofitnloss$ </p>
-  <p> Effective profit or loss of dealer: $effectiveprofitnloss$ </p>
-  
+  <p> Average buy price in the period: {averageBuy} </p>
+  <p> Average end price in the period: {averageSell} </p>
+  </div>
+}
   
 </div>
 
